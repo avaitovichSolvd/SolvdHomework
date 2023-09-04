d264f24947fec02779d7a6b294eac3fd9e207636
AlinaInFE <alinavfrontend@gmail.com>
2023-09-04 13:26:55 +0200

    Bonus

--- nodejs/eventLoop.js
+++ nodejs/eventLoop.js
@@ -1,4 +1,19 @@
 class AsyncOperationManager {
+  constructor() {
+    this.bonus = [];
+  }
+  
+  bonusSimulateAsyncOperation(callback, delay) {
+    this.bonus.push({ callback, delay });
+  }
+
+  start() {
+    this.bonus.forEach((operation) => {
+      setTimeout(() => {
+        operation.callback();
+      }, operation.delay);
+    });
+  }
 
   simulateAsyncOperation(delay) {
     setTimeout(() => {
@@ -20,9 +35,9 @@ class AsyncOperationManager {
     });
   }
 
-  scheduleImmediate() {
+  scheduleImmediate(message) {
     setImmediate(() => {
-      console.log("scheduleImmediate");
+      console.log(message);
     });
   }
 }
@@ -35,7 +50,7 @@ process.nextTick(() => {
   console.log("Microtask executed immediately");
 });
 // scheduleImmediate() тоже отработает раньше simulateAsyncOperation()
-manager.scheduleImmediate();
+manager.scheduleImmediate("scheduleImmediate");
 
 // таким образом вывод выглядит(где первые 2 строки
 // выполнились сразу же, позже уже все 3 подряд в своём
@@ -45,3 +60,29 @@ manager.scheduleImmediate();
 // simulateAsyncOperation's delay: 5000ms
 // simulateAsyncOperation's microtaskSchedule: 5000 ms
 // scheduleImmediate
+
+
+manager.bonusSimulateAsyncOperation(() => {
+  console.log("First time bonus 3000ms");
+  manager.microtaskSchedule("First time bonus 'microtaskSchedule'");
+  manager.scheduleImmediate("First time bonus 'scheduleImmediate'");
+}, 3000);
+
+manager.bonusSimulateAsyncOperation(() => {
+  console.log("Second time bonus 6000ms");
+  manager.microtaskSchedule("Second time bonus 'microtaskSchedule'");
+  manager.scheduleImmediate("Second time bonus 'scheduleImmediate'");
+}, 6000);
+
+manager.bonusSimulateAsyncOperation(() => {
+  console.log("Third time bonus 0ms");
+  manager.microtaskSchedule("Third time bonus 'microtaskSchedule'");
+  manager.scheduleImmediate("Third time bonus 'scheduleImmediate'");
+}, 0);
+
+manager.start();
+
+manager.microtaskSchedule("microtaskSchedule 1");
+manager.scheduleImmediate("scheduleImmediate 1");
+manager.microtaskSchedule("microtaskSchedule 2");
+manager.scheduleImmediate("scheduleImmediate 2");
