9f68c83d994db888e6d6cd33df35c6a1cfc2a69b
AlinaInFE <alinavfrontend@gmail.com>
2023-10-20 18:24:38 +0200

    SingUp

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 ;
 
 const db = mysql.createConnection({
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
   password: 'w%2npu9^Nm&Ys!H',
   database: 'solvdLawDB'
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
 
 app.use(bodyparser.json())
 
-app.post("/api/register", (req, res) => {
+app.post("/api/register", async (req, res) => {
   try{
     const {username, password} = req.body
 
@@ -23,28 +23,39 @@ app.post("/api/register", (req, res) => {
       return res.status(400).json({error: "All fields must be filled in"})
     }
 
-    const checkExisting = "SELECT * FROM users WHERE username = ?"
-    db.query(checkExisting, [username], (err, result) => {
-      if(err){
-        return res.status(500).json({error: "Checking failed"})
-      }
-      if(result.length > 0) {
-        return res.status(400).json({error: "User with this name already exist"})
-      }
+    const checkExisting = "SELECT * FROM user WHERE username = ?"
+    const [existingUsers] = await db.promise().query(checkExisting, [username]);
 
-      const newUser = "INSERT INTO users (username, password) VALUES (?, ?)"
-      db.query(newUser, [username, password], (err, results) => {
-        if (err) {
-          return res.status(500).json({error: "Operation adding new user has failed"})
-        }
-        return res.status(201).json({message: "Operation add new user went succsesful"})
-      })
-    })
+    if (existingUsers.length > 0) {
+      return res.status(400).json({ error: "User with this name already exists" });
+    }
+
+    const newUser = "INSERT INTO user (username, password) VALUES (?, ?)";
+    const [results] = await db.promise().query(newUser, [username, password]);
+
+    if (results.affectedRows === 1) {
+      return res.status(201).json({ message: "Operation to add a new user was successful" });
+    } else {
+      return res.status(500).json({ error: "Operation to add a new user failed" });
+    }
   } catch (err) {
-    console.log(err)
-    return res.status(500).json({error: "Operation has failed"})
+    console.log(err);
+    return res.status(500).json({ error: "Operation has failed" });
   }
-})
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const getUsersQuery = "SELECT * FROM user";
+    const [results, fields] = await db.promise().query(getUsersQuery);
+
+    return res.status(200).json({ users: results });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server started ${port}`);
