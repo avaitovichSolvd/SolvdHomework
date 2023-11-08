ca09044741d88b8988dcda400305853d71067c7f
AlinaInFE <alinavfrontend@gmail.com>
2023-11-08 18:36:21 +0100

    DOCKER

--- backend/db/DBkey.js
+++ backend/db/DBkey.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "w%2npu9^Nm&Ys!H",
+    password: "YCABtPLrKbCk",
     database: "solvdLawDB",
 });
 
