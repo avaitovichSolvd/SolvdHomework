bc0fd893499cd582fabe8e7611603668617817c9
AlinaInFE <alinavfrontend@gmail.com>
2023-11-10 10:41:19 +0100

    DockerComposeERROR

--- backend/db/DBkey.js
+++ backend/db/DBkey.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "YCABtPLrKbCk",
-  database: process.env.DB_NAME || "solvdLawDB",
+  host: process.env.DB_HOST,
+  // || "localhost",
+  user: process.env.DB_USER,
+  //  || "root",
+  password: process.env.DB_PASSWORD,
+  //  || "YCABtPLrKbCk",
+  database: process.env.DB_NAME,
+  // || "solvdLawDB",
 });
 
 module.exports = db;
