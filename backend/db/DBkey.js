f95cbe6427406ea76c85f1859eba17a3cfa1985f
AlinaInFE <alinavfrontend@gmail.com>
2023-11-13 16:16:59 +0100

    dockerCompose

--- backend/db/DBkey.js
+++ backend/db/DBkey.js
@@ -2,13 +2,9 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  // || "localhost",
   user: process.env.DB_USER,
-  //  || "root",
   password: process.env.DB_PASSWORD,
-  //  || "YCABtPLrKbCk",
   database: process.env.DB_NAME,
-  // || "solvdLawDB",
 });
 
 module.exports = db;
